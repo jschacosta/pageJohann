a47feb84526a5d57bbb2ce54af7a709aee8ff827
JohannSch <jschacosta@gmail.com>
2024-02-09 23:37:43 -0300

    primer commit con empaquetado

---
+++ script.js
@@ -0,0 +1,130 @@
+import { text } from "./text.js";
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+// Agrega la función al objeto global window
+window.toggleMenu = toggleMenu;
+
+// Segunda función:carrusel de fotos
+
+function initSlides() {
+  const slides = Array.from(document.querySelectorAll(".slide"));
+  let currentIndex = 0;
+
+  function nextSlide() {
+    // Elimina la clase 'active' de la diapositiva actual
+    slides[currentIndex].classList.remove("active");
+
+    // Incrementa el índice
+    currentIndex++;
+
+    // Si el índice es mayor que el número de diapositivas, vuelve a 0
+    if (currentIndex >= slides.length) {
+      currentIndex = 0;
+    }
+
+    // Añade la clase 'active' a la nueva diapositiva
+    slides[currentIndex].classList.add("active");
+  }
+
+  // Inicia el carrusel
+  slides[currentIndex].classList.add("active");
+  setInterval(nextSlide, 6000); // Cambia la diapositiva cada 3 segundos
+}
+
+function setTexts() {
+  // Primero, selecciona el elemento .aboutDescription
+  var aboutDescription = document.querySelector(".about-description");
+
+  // Luego, selecciona todos los elementos .cardAbout
+  var cards = document.querySelectorAll(".cardAbout");
+
+  // Para cada tarjeta, añade un event listener para el evento 'mouseover'
+  cards.forEach(function (card) {
+    card.addEventListener("mouseover", function () {
+      // Cuando el mouse pasa sobre la tarjeta, cambia el contenido de .aboutDescription
+      // basado en el id de la tarjeta
+      switch (card.id) {
+        case "card1":
+          aboutDescription.textContent = text.text1.en;
+          break;
+        case "card2":
+          aboutDescription.textContent = text.text2.en;
+          break;
+        case "card3":
+          aboutDescription.textContent = text.text3.en;
+          break;
+      }
+    });
+  });
+
+  document.addEventListener("click", function (event) {
+    // Si el elemento clickeado no es una tarjeta, limpia el texto de .aboutDescription
+    if (!event.target.closest(".cardAbout")) {
+      aboutDescription.textContent = "";
+    }
+  });
+}
+
+function scrolling() {
+  let sections = document.querySelectorAll("section");
+  let offsetAdjustment = window.innerHeight * 0.2; // Ajusta este valor según sea necesario
+  window.onscroll = () => {
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - offsetAdjustment;
+      let height = sec.offsetHeight;
+      if (top >= offset && top < offset + height) {
+        sec.classList.add("show-animate");
+      } else {
+        sec.classList.remove("show-animate");
+      }
+    });
+  };
+}
+
+function arrowsScroll() {
+  // Obtén los iconos de flecha
+  let arrowUp = document.querySelector(".arrowUp");
+  let arrowDown = document.querySelector(".arrowDown");
+  console.log(arrowDown);
+
+  // Agrega un controlador de eventos al icono de flecha hacia arriba
+  arrowUp.addEventListener("click", () => {
+    console.log("click1");
+    // Encuentra la sección actual
+    let currentSection = document.querySelector(".show-animate");
+    // Encuentra la sección anterior
+    let previousSection = currentSection.previousElementSibling;
+    // Si hay una sección anterior, desplázate hasta ella
+    if (previousSection) {
+      previousSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+
+  // Agrega un controlador de eventos al icono de flecha hacia abajo
+  arrowDown.addEventListener("click", () => {
+    console.log("click2");
+    // Encuentra la sección actual
+    let currentSection = document.querySelector(".show-animate");
+    // Encuentra la sección siguiente
+    let nextSection = currentSection.nextElementSibling;
+    // Si hay una sección siguiente, desplázate hasta ella
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+}
+
+function initAll() {
+  scrolling();
+  initSlides();
+  setTexts();
+  arrowsScroll();
+}
+
+// Llama a la función init cuando el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", initAll);
---
+++ text.js
@@ -0,0 +1,77 @@
+export const text = {
+  text1: {
+    en: `During the pandemic, I embarked on a self-taught journey into
+      programming, which ultimately led me to secure a position where
+      I've continued to expand my skill set. Over the past two years,
+      I've thrived in a remote work environment, mastering various
+      technologies and assuming leadership roles within my team as a
+      planner and coordinator. Additionally, I've embarked on a
+      personal mini-project, further demonstrating my passion for
+      continuous learning and innovation in the field.`,
+    esss: "Hola",
+    frff: "Bonjour",
+  },
+  text2: {
+    en: `I studied Mechanical Civil Engineering for six years in Chile,
+      where I gained valuable knowledge and practical experience. I
+      was actively involved as a sports delegate and served as a
+      teaching assistant. My education not only equipped me with
+      engineering expertise but also honed my analytical thinking
+      skills, essential for problem-solving and innovation.`,
+    esss: "Hola",
+    frss: "Bonjour",
+  },
+  text3: {
+    en: `As a full-stack developer, I specialize in Vue.js and React for dynamic user interfaces, advanced JavaScript for robust server-side applications, and non-relational databases for efficient data management. I frequently use AWS tools to deploy and maintain applications, ensuring optimal performance in cloud environments.`,
+    es: "Hola",
+    fr: "Bonjour",
+  },
+  text4: {
+    en: `As team leader for a two-year web application project, I've refined
+      leadership, coordination, and communication skills, ensuring project
+      success amidst challenges.`,
+    esss: "Hola",
+    frff: "Bonjour",
+  },
+  text5: {
+    en: ` Apuesta Laucha is my first fully developed web application. It allows
+      users to join tournaments where they predict sports outcomes,
+      earning points for correct guesses. The app includes a leaderboard
+      to track participants' scores, fostering competitive engagement.
+      This project showcases my ability to create comprehensive and
+      interactive solutions, highlighting my growth as a developer.`,
+    esss: "Hola",
+    frrr: "Bonjour",
+  },
+  text6: {
+    en: ` SOS travelers, is a web application facilitating service bookings,
+      such as barber appointments, massages, and tattoos, across various
+      hostels and hotels. This platform serves as a bridge between users,
+      service providers, and accommodations, offering tailored versions
+      for each user type. It features online payment functionality and an
+      integrated internal chat system for seamless communication.`,
+    esss: "Hola",
+    frff: "Bonjour",
+  },
+  text7: {
+    en: `Unabase grid, a customizable web grid was developed, similar to a
+      personalized Excel sheet, allowing users to generate business
+      quotations. This project was a collaborative effort with a team of
+      three, emphasizing teamwork in achieving successful implementation.`,
+    esss: "Hola",
+    frrr: "Bonjour",
+  },
+  text8: {
+    en: `I developed a website for a ceramic artist to showcase her portfolio
+      effectively. The emphasis was on designing a visually appealing
+      platform that reflects her aesthetic and serves as a compelling
+      introduction to her work.`,
+    esss: "Hola",
+    frrr: "Bonjour",
+  },
+  text9: {
+    en: `I'm a Chilean who speaks Spanish fluently, having lived in Berlin for over 6 months, where I honed my English skills through travel and immersion. Additionally, I'm proficient in Portuguese, having also spent extended periods of time in Portuguese-speaking regions.`,
+    esss: "Hola",
+    frrr: "Bonjour",
+  },
+};
